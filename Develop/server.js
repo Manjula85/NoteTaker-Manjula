e82865426e3fab8afa7bb8cabad1f65e1120e4ab
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-02-21 14:34:15 -0500

    update v1

--- Develop/server.js
+++ Develop/server.js
@@ -9,6 +9,6 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-app.get('/', (req, res){
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
