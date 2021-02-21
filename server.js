5a645f5501ee487ebfe85ed24d02afa0a3a7354d
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-02-21 15:14:12 -0500

    update v2

---
+++ server.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+    //res.send("test");
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+//app.get('/')
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
